48928e49b8b86014d3087bf44c2a56e4c7e432a4
Waldo Nazco <alu0100537185@ull.edu.es>
2013-10-18 15:06:15 +0100

    Añadiendo codigo al programa racional.rb

--- racional.rb
+++ racional.rb
@@ -3,4 +3,80 @@
 require "./gcd.rb"
 
 class Fraccion
-end
+
+# Método para inicializar una clase
+  
+    def initialize (numerador,denominador)
+      # atributos      
+      @numerador = numerador;
+      @denominador = denominador; 
+    end
+
+# Método para convertir una cadena
+    
+    def to_s 
+	"#{@numerador}/#{@denominador}"
+    end
+
+# Método para suma de fracciones
+  
+    def suma(a,b) 
+      "#{@numerador}/#{@denominador} + #{a}/#{b} ="
+      temp = (@denominador/gcd(@denominador,b)) * b # Calculamos el mínimo común múltiplo utilizando la propiedad (a*b/mcd(a,b))
+      temp2 = ((temp/@denominador) * @numerador) + ((temp/b) * a) # Calculamos la suma de fracciones
+      # Mostramos el resultado por pantalla     
+      if temp2 % temp == 0 # Mostramos el resultado de la división comprobando antes que la división es exacta o no.
+	 temp3 = temp2/temp
+      else
+         "#{temp2}/#{temp}"
+      end  
+    end
+   
+# Método para resta de fracciones
+   
+    def resta(a,b) 
+      "#{@numerador}/#{@denominador} + #{a}/#{b} ="
+      temp = (@denominador/gcd(@denominador,b)) * b # Calculamos el mínimo común múltiplo utilizando la propiedad (a*b/mcd(a,b)) 
+      temp2 = ((temp/@denominador) * @numerador) - ((temp/b) * a) # Calculamos la resta de fracciones
+      # Mostramos el resultado por pantalla
+      if temp2 % temp == 0 # Mostramos el resultado de la división comprobando antes que la división es exacta o no.
+	 temp3 = temp2/temp
+         "#{temp3}"
+      else
+         "#{temp2}/#{temp}"
+      end
+    end   
+   
+
+# Método para el producto de fracciones
+    
+    def producto(a,b)
+      "#{@numerador}/#{@denominador} * #{a}/#{b} ="
+      # Calculamos el producto de fracciones
+      temp = @denominador * b
+      temp2 = @numerador * a
+      # Mostramos el resultado por pantall
+      if temp2 % temp == 0 # Mostramos el resultado de la división comprobando antes que la división es exacta o no.
+         temp2/temp
+      else
+         "#{temp2}/#{temp}"
+      end
+    end
+
+# Método para la divisíon de fracciones
+        
+    def division(a,b) 
+      "#{@numerador}/#{@denominador} / #{a}/#{b} ="
+       # Calculamos la multiplicación en cruz de las fracciones
+       temp = @denominador * a
+       temp2 = @numerador * b
+       # Mostramos el resultado por pantalla
+       if temp2 % temp == 0 # Mostramos el resultado de la división comprobando antes que la división es exacta o no.
+
+          temp2/temp
+       else
+          "#{temp2}/#{temp}"
+       end
+     end
+end  
+
